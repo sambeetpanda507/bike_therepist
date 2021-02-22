00bfee6739b8bb7823bd0e6b87cdf945a6a8824e
sambeetpanda507 <sambeetpanda507@gmail.com>
2021-02-22 23:27:44 +0530

    added landing page

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,12 +1,18 @@
-import style from "../styles/LandingStyle.css";
+import style from "../styles/Landing.module.css";
 
 const Landing = () => {
   return (
     <section className={style.landing} id="landing">
-      <h1>
-        <span>mechanic</span> at your <br /> doorstep
+      <h1 className={style.headingPrimary}>
+        <span className={style.mechanic}>mechanic</span> at your <br />{" "}
+        <span className={style.door}>doorstep</span>
       </h1>
-      <h3>At Delhi & gurgaon</h3>
+      <h3 className={style.headingSecondary}>At Delhi & gurgaon</h3>
+      <div className={style.button}>
+        <button>book now &rarr;</button>
+      </div>
     </section>
   );
 };
+
+export default Landing;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Landing from "../components/Landing";
 export default function Home() {
   return (
     <section id="home">
@@ -10,7 +10,7 @@ export default function Home() {
           content="Bike therapist is a website which provides complete service for a bike"
         />
       </Head>
-      <h1>this is the home page</h1>
+      <Landing />
     </section>
   );
 }
