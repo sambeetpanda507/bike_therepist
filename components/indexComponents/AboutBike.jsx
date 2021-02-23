6d15332e6db3c7c5bf08e579e3752a256b108d39
sambeetpanda507 <sambeetpanda507@gmail.com>
2021-02-23 16:11:18 +0530

    updated about bike page

--- components/indexComponents/AboutBike.jsx
+++ components/indexComponents/AboutBike.jsx
@@ -4,18 +4,23 @@ const AboutBike = () => {
   return (
     <section className={style.aboutBike} id="aboutBike">
       <div className={style.container}>
-        <h1 className={style.primaryHeading}>
+        <h1 className={`${style.primaryHeading} ${style.showMd}`}>
           about <span className={style.bike}>bike</span>{" "}
           <span className={style.textDanger}>therapist</span>
         </h1>
         <div className={style.containerContent}>
           <div className={style.actualContent}>
+            <h1 className={`${style.primaryHeading} ${style.hideLg}`}>
+              about <span className={style.bike}>bike</span>{" "}
+              <span className={style.textDanger}>therapist</span>
+            </h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
               eaque quod praesentium iusto suscipit pariatur inventore
               consectetur ea deserunt aut quae adipisci veniam doloremque
               laborum illo aspernatur! Impedit, molestias enim.
             </p>
+            <br />
             <br />
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
