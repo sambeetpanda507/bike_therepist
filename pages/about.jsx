d0317d3e384331a0c560423532b6a1013f60828b
sambeetpanda507 <sambeetpanda507@gmail.com>
2021-02-25 19:20:51 +0530

    completed service page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Landing from "../components/aboutComponents/Landing";
 import Engine from "../components/aboutComponents/Engine";
 import Worker from "../components/aboutComponents/Worker";
@@ -5,6 +6,13 @@ import Welding from "../components/aboutComponents/Welding";
 const About = () => {
   return (
     <section id="about">
+      <Head>
+        <title>About Us</title>
+        <meta
+          name="description"
+          content="this is the about us page for bike therapist"
+        />
+      </Head>
       <Landing />
       <Engine />
       <Worker />
