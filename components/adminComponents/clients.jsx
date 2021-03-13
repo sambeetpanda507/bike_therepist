cc693d3a42df51d0cf0b055af38fe99c169843f3
sambeetpanda507 <sambeetpanda507@gmail.com>
2021-03-13 15:00:24 +0530

    added status to admin page

--- components/adminComponents/clients.jsx
+++ components/adminComponents/clients.jsx
@@ -1,66 +1,74 @@
+import moment from "moment";
 import styles from "../../styles/adminDashboard/invoices.module.css";
 
-const Payments = () => {
+const Payments = ({ clientsData }) => {
   return (
     <div className={styles.invoices}>
-      <div className={styles.invoice}>
-        <div>
-          <h1>Clients</h1>
-          <p>Dashboard / Clients</p>
+      <fieldset>
+        <div className={styles.invoice}>
+          <div>
+            <legend>
+              <h1>Clients</h1>
+              <p>Dashboard / Clients</p>
+            </legend>
+          </div>
         </div>
-      </div>
-      <div className={styles.search}>
-        <div className={styles.wrapper}>
-          <input type="text" required />
-          <label>Client ID</label>
+        <div className={styles.search}>
+          <div className={styles.wrapper}>
+            <input type="text" list="clientId" required />
+            <label>Client ID</label>
+            <datalist id="clientId">
+              {clientsData.map((client, index) => {
+                return <option value={client._id} key={index} />;
+              })}
+            </datalist>
+          </div>
+          <div className={styles.wrapper}>
+            <input type="text" list="clientName" required />
+            <label>Name</label>
+            <datalist id="clientName">
+              {clientsData.map((client, index) => {
+                return <option value={client.name} key={index} />;
+              })}
+            </datalist>
+          </div>
+          <div className={styles.btn}>
+            <i className="fas fa-search"></i> &nbsp;&nbsp;SEARCH
+          </div>
         </div>
-        <div className={styles.wrapper}>
-          <input type="text" required />
-          <label>Name</label>
-        </div>
-        <div className={styles.btn}>
-          <i class="fas fa-search"></i> &nbsp;&nbsp;SEARCH
-        </div>
-      </div>
+      </fieldset>
       <div className={styles.table3}>
-        <table>
-          <tr>
-            <th>Client ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Brand</th>
-            <th>Varient</th>
-            <th>Date</th>
-            <th>Timing</th>
-            <th>Phone</th>
-            <th>Status</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Client ID</td>
-            <td>Name</td>
-            <td>Paid Date</td>
-            <td>Total</td>
-            <td>Status</td>
-            <td>5pm</td>
-            <td>9876543210</td>
-            <td>
-              <button>Pending</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Client ID</td>
-            <td>Name</td>
-            <td>Paid Date</td>
-            <td>Total</td>
-            <td>Status</td>
-            <td>5pm</td>
-            <td>9876543210</td>
-            <td>
-              <button>Pending</button>
-            </td>
-          </tr>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Client ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Brand</th>
+              <th>Varient</th>
+              <th>Date</th>
+              <th>Timing</th>
+              <th>Phone</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientsData.map((client, index) => {
+              return (
+                <tr key={index}>
+                  <td>{client._id}</td>
+                  <td>{client.name}</td>
+                  <td>{client.email}</td>
+                  <td>{client.brand}</td>
+                  <td>{client.variant}</td>
+                  <td>{moment(client.bookingDate).format("MMMM Do YYYY")}</td>
+                  <td>{client.bookingTime}</td>
+                  <td>{client.phone}</td>
+                  <td>{client.status}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
