ecebdae170796430550a846fc0401fcbac70427e
sambeetpanda507 <sambeetpanda507@gmail.com>
2021-03-15 15:24:17 +0530

    populate Payment component table

--- pages/payments.jsx
+++ pages/payments.jsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import SidePanel from "../components/adminComponents/SidePanel";
 import Payments from "../components/adminComponents/payments";
+import { parseCookies } from "nookies";
 import styles from "../styles/adminDashboard/sidepanel.module.css";
 
-const adminDashBoard = () => {
+const adminDashBoard = ({ paymentData }) => {
   return (
     <section id="dashboard">
       <Head>
@@ -15,10 +16,33 @@ const adminDashBoard = () => {
       </Head>
       <div className={styles.main}>
         <SidePanel />
-        <Payments />
+        <Payments paymentData={paymentData} />
       </div>
     </section>
   );
 };
+
+export async function getServerSideProps(context) {
+  const cookies = parseCookies(context);
+  if (cookies) {
+    if (!cookies.jwt) {
+      context.res.writeHead(302, { Location: "/" });
+      context.res.end();
+    }
+  } else {
+    context.res.writeHead(302, { Location: "/" });
+    context.res.end();
+  }
+  const paymentRes = await fetch("http://localhost:8080/api/payments", {
+    method: "GET",
+    mode: "cors",
+    credentials: "same-origin",
+  });
+
+  const paymentData = await paymentRes.json();
+  return {
+    props: { paymentData },
+  };
+}
 
 export default adminDashBoard;
